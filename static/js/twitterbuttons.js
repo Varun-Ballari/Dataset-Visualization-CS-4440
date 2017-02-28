6b9dcd7c5bcf096999642220abe75bfb0947d9d9
Varun Ballari <varun@ballari.com>
2017-02-28 16:25:22 -0500

    Made backend and some frontend

---
+++ static/js/twitterbuttons.js
@@ -0,0 +1,17 @@
+// var twitterBtns = document.querySelectorAll('.twitter');
+//
+// for (var i = 0; i < twitterBtns.length; i++) {
+//   twitterBtns[i].addEventListener('click', twitterClick);
+// }
+//
+// function twitterClick(e) {
+//   this.dataset.pressed = toggle(this.dataset.pressed);
+// }
+//
+// function toggle(value) {
+//   if (value === 'true') {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
